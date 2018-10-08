2e0662fdfef59b0b2334de5a6f96504a22310d0a
yenesey <d.enisei@yandex.ru>
2018-10-08 10:20:51 +0300

    first

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <tinymce id="d1" v-model="data"></tinymce>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import tinymce from 'vue-tinymce-editor'
+Vue.component('tinymce', tinymce)
+
+export default{
+    data(){
+        return {
+            data : ''
+        };
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
